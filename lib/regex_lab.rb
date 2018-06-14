397b99a41f4fa226e4647fdaf1e9a14d105a7e0b
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-14 11:50:00 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,19 @@
 def starts_with_a_vowel?(word)
-
+!!(word.match(/\b[aeiouAEIOU]w*/))
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+text.scan(/\b+un\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+text.scan(/\w{5}$/)
 end
 
-def first_word_capitalized_and_ends_with_punctuation?(text)
+#def first_word_capitalized_and_ends_with_punctuation?(text)
 
-end
+#end
 
-def valid_phone_number?(phone)
+#def valid_phone_number?(phone)
 
-end
+#end
