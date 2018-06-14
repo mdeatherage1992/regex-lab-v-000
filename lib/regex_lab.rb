bd68a174353f53e8f7f51d080987d7f97bdc2999
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-14 12:54:35 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,5 +15,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-!!(phone.match(/^\S\d+.\d+.\d{3}$/))
+!!(phone.match(/^\S\d+.\d{3}.\d{2,4}/))
 end
